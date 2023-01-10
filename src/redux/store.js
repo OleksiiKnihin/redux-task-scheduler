0be29e7e881e046b3fe01e419fbd5c11b5e1c62f
Aleksey Knigin <alexyo2626@gmail.com>
2023-01-10 17:05:45 +0200

    finish with redux

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux';
+import { devToolsEnhancer } from '@redux-devtools/extension';
+import { rootReducer } from './reducer';
+
+// Создаем расширение стора чтобы добавить инструменты разработчика
+const enhancer = devToolsEnhancer();
+
+export const store = createStore(rootReducer, enhancer);
